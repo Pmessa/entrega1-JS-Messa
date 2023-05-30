532f61b620a6f1bf8286d771d6f288cde94c13fa
Pablo Messa <130992694+Pmessa@users.noreply.github.com>
2023-05-30 16:21:48 -0300

    Primeras pruebas de código

--- PreEntrega1Messa.js
+++ PreEntrega1Messa.js
@@ -8,3 +8,25 @@
 //https://github.com/Pmessa/entrega-final-pablo-messa.git
 
 
+let seccion = prompt("Bienvenido a nuesta plataforma de compras, ingrese la sección en que desea comprar 1- para asesorías y 2- para cursos")
+
+switch (seccion) {
+    case "1":
+        console.log("Usted se encuentra en la sección ASESORÍAS")
+        alert("Usted se encuentra en la sección ASESORÍAS presione la tecla Enter para seleccionar la asesoría de su interés")
+        break;
+    case "2":
+        console.log("Usted se encuentra en la sección Cursos")
+        alert("Usted se encuentra en la sección Cursos presione la tecla Enter para seleccionar el curso de su interés")
+        break;
+    default:
+        prompt("El valor ingresado no es correcto por favor ingrese 1- para asesorías y 2- para cursos")
+
+}
+
+if (seccion) {
+    console.log("Usted se encuentra en la sección Cursos")
+    alert("Usted se encuentra en la sección Cursos presione la tecla Enter para seleccionar el curso de su interés")
+} else {
+    alert("El valor ingresado no es correcto")
+}
