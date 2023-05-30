d5e0d0895266a0486b78b3a40c267c1c19d508a7
Pablo Messa <130992694+Pmessa@users.noreply.github.com>
2023-05-30 16:55:34 -0300

    primeras pruebas

--- PreEntrega1Messa.js
+++ PreEntrega1Messa.js
@@ -9,24 +9,25 @@
 
 
 let seccion = prompt("Bienvenido a nuesta plataforma de compras, ingrese la sección en que desea comprar 1- para asesorías y 2- para cursos")
-
-switch (seccion) {
-    case "1":
-        console.log("Usted se encuentra en la sección ASESORÍAS")
-        alert("Usted se encuentra en la sección ASESORÍAS presione la tecla Enter para seleccionar la asesoría de su interés")
-        break;
-    case "2":
-        console.log("Usted se encuentra en la sección Cursos")
-        alert("Usted se encuentra en la sección Cursos presione la tecla Enter para seleccionar el curso de su interés")
-        break;
-    default:
-        prompt("El valor ingresado no es correcto por favor ingrese 1- para asesorías y 2- para cursos")
-
+while (seccion != 1,2){
+    switch (seccion){
+        case "1": console.log("Usted se encuentra en la sección ASESORÍAS")
+            alert("Usted se encuentra en la sección ASESORÍAS presione la tecla Enter para seleccionar la asesoría de su interés")
+            break
+        
+    
+        case "2": console.log("Usted se encuentra en la sección Cursos")
+            alert("Usted se encuentra en la sección Cursos presione la tecla Enter para seleccionar el curso de su interés")
+            break;
+        
+    
+        default: prompt("El valor ingresado no es correcto por favor ingrese 1- para asesorías y 2- para cursos")
+            break
+        
+     
+    }
+    seccion = prompt("Bienvenido a nuesta plataforma de compras, ingrese la sección en que desea comprar 1- para asesorías y 2- para cursos")
 }
 
-if (seccion) {
-    console.log("Usted se encuentra en la sección Cursos")
-    alert("Usted se encuentra en la sección Cursos presione la tecla Enter para seleccionar el curso de su interés")
-} else {
-    alert("El valor ingresado no es correcto")
-}
+
+
