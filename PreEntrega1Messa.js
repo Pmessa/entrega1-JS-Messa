b567d8a0b0518d51f93a5796adf4d9d8835fc97d
Pmessa <pablomessa@gmail.com>
2023-05-30 09:17:55 -0300

    first commit

---
+++ PreEntrega1Messa.js
@@ -0,0 +1,10 @@
+//Primera Entrega Pablo Messa
+
+//En la entrega final del curso de desarrollo web realice una página a la que debería darle funcionalidad
+
+//La idea es que los usuarios puedan elegir y agregar al carrito(que todavía no existe), los productos disponibles que tampoco existen aún.
+
+//El proyecto está en mi repositorio de Github.
+//https://github.com/Pmessa/entrega-final-pablo-messa.git
+
+
