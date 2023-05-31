861efa3978e5f9b95f7b3791a6e006593438a5a5
Pablo Messa <130992694+Pmessa@users.noreply.github.com>
2023-05-30 23:37:21 -0300

    30 de mayo

--- PreEntrega1Messa.js
+++ PreEntrega1Messa.js
@@ -7,27 +7,22 @@
 //El proyecto está en mi repositorio de Github.
 //https://github.com/Pmessa/entrega-final-pablo-messa.git
 
-
-let seccion = prompt("Bienvenido a nuesta plataforma de compras, ingrese la sección en que desea comprar 1- para asesorías y 2- para cursos")
-while (seccion != 1,2){
-    switch (seccion){
-        case "1": console.log("Usted se encuentra en la sección ASESORÍAS")
-            alert("Usted se encuentra en la sección ASESORÍAS presione la tecla Enter para seleccionar la asesoría de su interés")
-            break
-        
-    
-        case "2": console.log("Usted se encuentra en la sección Cursos")
-            alert("Usted se encuentra en la sección Cursos presione la tecla Enter para seleccionar el curso de su interés")
-            break;
-        
-    
-        default: prompt("El valor ingresado no es correcto por favor ingrese 1- para asesorías y 2- para cursos")
-            break
-        
-     
-    }
-    seccion = prompt("Bienvenido a nuesta plataforma de compras, ingrese la sección en que desea comprar 1- para asesorías y 2- para cursos")
+let entrada = prompt ("ingrese 1 para asesorías o 2 para Cursos ingrese 0 para salir del menú");
+while (entrada !="0") {
+    //Repetimos hasta que se ingresa "ESC"
+        switch(entrada){
+            case "1":
+                console.log(`Asesorías`)
+                if (entrada ==1){
+                   prompt("elija una opción de asesoría")
+                }
+               break;
+            case "2":
+                console.log(`Cursos`)
+                break;
+            default :
+                console.log(`${entrada} no es una opción válida`)
+                break;
+            }
+            entrada = prompt("Ingrese un número válido")
 }
-
-
-
