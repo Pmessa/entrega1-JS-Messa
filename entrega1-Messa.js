0cebddf1533566b3553cd6476a35045047782fe8
pmessa <pablomessa@gmail.com>
2023-06-12 23:39:44 -0300

    Simulador corregido

--- entrega1-Messa.js
+++ entrega1-Messa.js
@@ -1,142 +1,51 @@
 //Pre Entrega 1 Pablo Messa
+let mtbSpecialized = 3000
+let mtbMerida = 3850
+let mtbScott = 3600
+let rutaSpecialized = 3850
+let rutaMerida = 3850
+let rutaScott = 2900
 
+let itemSeleccionado = parseInt(prompt(`Seleccione una opción
+1-Bicicleta MTB
+2-Bicicleta RUTA
+3-Salir del menú`))
 
-function validarEntrada(opcion) {
-    while (isNaN(opcion)) {
-        opcion = parseInt(prompt("ATENCIÓN INGRESE EL VALOR NUEVAMENTE 1- MTB 2- RUTA 3- Salir del menú"))
-    } return opcion
+function enviaremos(){
+    console.log(`Le enviaremos a su mail la mejor oferta para que pueda acceder a su nueva bici!!!`)
 }
-const biciMtb =[
-let mtbRoja = (100000)
-const bicimtbRoja = {
-    tipo:"mtb",
-    color:"rojo",
-    precio:100000,
-    rodado:29,
-    talle:"m"
-}
-const bicimtbVerde = {
-    tipo:"mtb",
-    color:"verde",
-    precio:200000,
-    rodado:29,
-    talle:"m"
-}
-const bicimtbAzul = {
-    tipo:"mtb",
-    color:"Azul",
-    precio:300000,
-    rodado:29,
-    talle:"m"
-}
-]
-let mtbVerde = (200000)
-let mtbAzul = (300000)
-let rutaBlanca = (120000)
-let rutaGris = (250000)
-let rutaNegra = (360000)
-
-let tipoBici = prompt(`Bienvenido a nuestra sección bicicletas!
-SELECCIONE LA OPCIÓN DESEADA.
-1- MTB
-2- RUTA
-3- Salir del menú`)
-
-while (tipoBici < 1 && tipoBici > 3) {
-    alert("La opción ingresada no es válida")
-    tipoBici = parseInt(prompt(`Ingrese el número de la opción deseada:
-         1- MTB
-         2- RUTA
-         3- Salir del menú`))
-}
-let opcionValidada = validarEntrada(tipoBici)
-if (opcionValidada == 1) {
-    let mtb = parseInt(prompt(`Seleccione su bici MTB
-        1-Roja $100.000-
-        2-Verde $200.000-
-        3-Azul $300.000-`))
-    let precio
-    switch (mtb) {
-        case 1: alert((`Usted ha seleccionado la MTB roja de $100.000- 
-            Presione "Enter" para continuar`))
-            roja = parseInt(prompt(`Ingrese forma de pago
-            1 - Efectivo (10% de descuento)
-            2 - 12 Cuotas con Tarjeta de crédito (Precio de lista + 50%)`))
-            if (roja == 1) {
-                precio = alert(`El monto a abonar es de $${mtbRoja * 0.9}`)
-            }
-            else if (roja == 2) {
-                precio = alert(`El costo de su MTB Roja es de 12 cuotas de  $${(mtbRoja * 1.5) / 12}`)
-            }
-            break
-        case 2: alert((`Usted ha seleccionado la MTB verde de $200.000- 
-            Presione "Enter" para continuar`))
-            verde = parseInt(prompt(`Ingrese forma de pago
-            1 - Efectivo (10% de descuento)
-            2 - 12 Cuotas con Tarjeta de crédito (Precio de lista + 50%)`))
-            if (verde == 1) {
-                precio = alert(`El monto a abonar es de $${mtbVerde * 0.9}`)
-            }
-            else if (verde == 2) {
-                precio = alert(`El costo de su MTB Verde es de 12 cuotas de  $${(mtbVerde * 1.5) / 12}`)
-            }
-            break
-        case 3: alert((`Usted ha seleccionado la MTB Azul de $300.000- 
-            Presione "Enter" para continuar`))
-            azul = parseInt(prompt(`Ingrese forma de pago
-            1 - Efectivo (10% de descuento)
-            2 - 12 Cuotas con Tarjeta de crédito (Precio de lista + 50%)`))
-            if (azul == 1) {
-                precio = alert(`El monto a abonar es de $${mtbAzul * 0.9}`)
-            }
-            else if (azul == 2) {
-                precio = alert(`El costo de su MTB Azul es de 12 cuotas de  $${(mtbAzul * 1.5) / 12}`)
-            }
-            break
+switch(itemSeleccionado){
+    case 1:
+    console.log(`Usted ha seleccionado Bicicletas MTB`)
+    let mtb= prompt(`seleccione la marca de su bicicleta MTB:
+    1- Specialized
+    2- Merida
+    3- Scott`)
+    if(mtb == 1){
+       enviaremos( alert(`Enviaremos su Specialized MTB en solo 12 cuoatas de  de U$D` + mtbSpecialized / 12))
+    }else if(mtb == 2){
+        enviaremos( alert(`Enviaremos su Specialized MTB en solo 12 cuoatas de  de U$D` + mtbMerida / 12))
+    }else if(mtb == 3){
+        enviaremos( alert(`Enviaremos su Specialized MTB en solo 12 cuoatas de  de U$D` + mtbScott / 12))
     }
-}
-else if (opcionValidada == 2) {
-    let ruta = parseInt(prompt(`Seleccione su bici de RUTA
-        1-Blanca $120.000-
-        2-Gris $250.000-
-        3-Negra $360.000-`))
-    switch (ruta) {
-        case 1: alert((`Usted ha seleccionado la bicicleta de ruta blanca de $120.000- 
-                Presione "Enter" para continuar`))
-            blanca = parseInt(prompt(`Ingrese forma de pago
-                1 - Efectivo (10% de descuento)
-                2 - 12 Cuotas con Tarjeta de crédito (Precio de lista + 50%)`))
-            if (blanca == 1) {
-                precio = alert(`El monto a abonar es de $${rutaBlanca * 0.9}`)
-            }
-            else if (blanca == 2) {
-                precio = alert(`El costo de bicicleta de ruta blanca es de 12 cuotas de  $${(rutaBlanca * 1.5) / 12}`)
-            }
-            break
-        case 2: alert(`Usted ha seleccionado la bicicleta de ruta gris de $250.000- 
-                Presione "Enter" para continuar`)
-            gris = parseInt(prompt(`Ingrese forma de pago
-                1 - Efectivo (10% de descuento)
-                2 - 12 Cuotas con Tarjeta de crédito (Precio de lista + 50%)`))
-            if (gris == 1) {
-                precio = alert(`El monto a abonar es de $${rutaGris * 0.9}`)
-            }
-            else if (gris == 2) {
-                precio = alert(`El costo de su bicicleta de ruta gris es de 12 cuotas de $${(rutaGris * 1.5) / 12}`)
-            }
-            break
-        case 3: alert((`Usted ha seleccionado la bicicleta de ruta negra de $360.000- 
-                Presione "Enter" para continuar`))
-            negra = parseInt(prompt(`Ingrese forma de pago
-                1 - Efectivo (10% de descuento)
-                2 - 12 Cuotas con Tarjeta de crédito (Precio de lista + 50%)`))
-            if (negra == 1) {
-                precio = alert(`El monto a abonar es de $${rutaNegra * 0.9}`)
-            }
-            else if (ruta == 2) {
-                precio = alert(`El costo de su MTB Roja es de 12 cuotas de  $${(rutaNegra * 1.5) / 12}`)
-            }
-            break
-    }
+    break
+    case 2:
+    console.log(`Usted ha seleccionado Bicicletas RUTA`)
+    let ruta= prompt(`seleccione la marca de su bici de RUTA:
+    1- Specialized
+    2- Merida
+    3- Scott`)
+    if(ruta == 1){
+        enviaremos( alert(`Enviaremos su Specialized MTB en solo 12 cuoatas de  de U$D` + rutaSpecialized / 12))
+     }else if(ruta == 2){
+        enviaremos( alert(`Enviaremos su Specialized MTB en solo 12 cuoatas de  de U$D` + rutaMerida / 12))
+     }else if(ruta == 3){
+        enviaremos( alert(`Enviaremos su Specialized MTB en solo 12 cuoatas de  de U$D` + rutaScott / 12))
+     }
+    break
+    default:
+    console.log(`La opcion no es correcta, Gracias por utilizar nuestra app, presine F5 para volver a ver el menú`)
+    break
+
 }
 
